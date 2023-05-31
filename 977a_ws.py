c1b2ccc9becc3dd1320f2d1f27227175cf6e964a
Ukeme Edet <ukemeedet2207@gmail.com>
2023-05-31 23:30:21 +0100

    Solved One

---
+++ 977a_ws.py
@@ -0,0 +1,12 @@
+def main():
+    n, k = map(int, input().split(" "))
+    for _ in range(k):
+        if n % 10 == 0:
+            n //= 10
+        else:
+            n -= 1
+    print(n)
+
+
+if __name__ == "__main__":
+    main()
