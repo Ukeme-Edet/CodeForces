64847cb12452ebc86efcccf62d5063df5e0f1292
Ukeme Edet <ukemeedet2207@gmail.com>
2023-07-16 15:03:34 +0100

    Solved one :)

--- 1842b_tab.py
+++ 1842b_tab.py
@@ -2,9 +2,21 @@ def main():
     t = int(input())
     for _ in range(t):
         n, x = map(int, input().split())
-        a = [int(x) for x in input().split()]
-        b = [int(x) for x in input().split()]
-        c = [int(x) for x in input().split()]
+        books = [
+            [int(x) for x in input().split()],
+            [int(x) for x in input().split()],
+            [int(x) for x in input().split()]
+        ]
+        k = 0
+        for i in range(3):
+            for j in range(n):
+                if x | books[i][j] != x:
+                    break
+                k |= books[i][j]
+        if k == x:
+            print("YES")
+        else:
+            print("NO")
 
 
 if __name__ == "__main__":
