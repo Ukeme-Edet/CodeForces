1343d9cd005b3d223df52a72426b97772b9c7615
Ukeme Edet <ukemeedet2207@gmail.com>
2023-07-28 23:57:51 +0100

    Attemped :/

---
+++ 1472c_lj.py
@@ -0,0 +1,34 @@
+from sys import stdin, stdout
+
+
+def input():
+    return stdin.readline().strip()
+
+
+def print(string):
+    return stdout.write(str(string) + "\n")
+
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = [int(x) for x in input().split()]
+        dp, max_s = [0] * n, 0
+        for i in range(n):
+            if not dp[i]:
+                stack, j = [], i
+                while j < n:
+                    stack.append((a[j], j))
+                    j += a[j]
+                ai, j = stack.pop()
+                dp[j] = a[j]
+                while stack:
+                    ai, j = stack.pop()
+                    dp[j] = dp[j + a[j]] + ai
+                max_s = max(max_s, dp[i])
+        print(max_s)
+
+
+if __name__ == "__main__":
+    main()
