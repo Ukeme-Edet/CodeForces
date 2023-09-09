549975ce100d2cc3ceb146d7f7b17a4c275a0671
Ukeme Edet <ukemeedet2207@gmail.com>
2023-09-09 19:04:48 +0100

    Solved one :)

--- 1742c_stripes.py
+++ 1742c_stripes.py
@@ -19,26 +19,14 @@ def main():
                 continue
             c += 1
             grid.append(s)
-        print(check_last(grid))
-
-
-def check_last(grid):
-    return check_cols(grid) or check_rows(grid)
-
-
-def check_cols(grid):
-    for i in range(8):
-        col = set([grid[j][i] for j in range(8)])
-        if len(col) == 1 and "." not in col:
-            return col.pop()
-    return None
+        print("R" if check_rows(grid) else "B")
 
 
 def check_rows(grid):
     for row in grid:
-        if len(set(row)) == 1 and "." not in row:
-            return row[0]
-    return None
+        if row.count("R") == 8:
+            return True
+    return False
 
 
 if __name__ == "__main__":
