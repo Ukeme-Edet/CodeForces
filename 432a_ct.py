b033c3263afde3fb61c4517b2433b5d71c80179b
Ukeme Edet <ukemeedet2207@gmail.com>
2023-11-06 11:25:00 +0100

    Solved one :)

---
+++ 432a_ct.py
@@ -0,0 +1,18 @@
+from sys import stdin, stdout
+
+
+def input():
+    return stdin.readline().strip()
+
+
+def print(string):
+    return stdout.write(str(string) + "\n")
+
+
+def main():
+    n, k = [int(x) for x in input().split()]
+    print(len([y for y in [int(x) for x in input().split()] if y <= 5 - k]) // 3)
+
+
+if __name__ == "__main__":
+    main()
