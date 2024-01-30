e11c51460d4b70b9fd736dc7156d053bc1f14675
Ukeme Edet <ukemeedet2207@gmail.com>
2024-01-31 00:25:51 +0100

    Solved one :)

---
+++ 630a_atf.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file 630a_atf.cpp
+ * @author Ukeme Edet (ukemeedet2207@gmail.com)
+ * @brief The solution to the problem 630A - Again Twenty Five!(https://codeforces.com/problemset/problem/630/A)
+ * @version 0.1
+ * @date 2024-01-31
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * @brief The main function
+ *
+ * @return int
+ */
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	short n;
+
+	cin >> n;
+	cout << 25;
+	return (EXIT_SUCCESS);
+}
