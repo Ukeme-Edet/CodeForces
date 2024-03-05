1e4b2496d02eddd0b70839ece53cb8a9b12d8688
Ukeme Edet <ukemeedet2207@gmail.com>
2024-03-05 01:12:09 +0100

    Solved one :)

--- 1904a_forked.cpp
+++ 1904a_forked.cpp
@@ -3,7 +3,7 @@
  * @author Ukeme Edet (ukemeedet2207@gmail.com)
  * @brief The solution to the problem Forked!(https://codeforces.com/problemset/problem/1904/A)
  * @version 0.1
- * @date 2024-03-05
+ * @date 2024-03-14
  *
  * @copyright Copyright (c) 2024
  *
@@ -22,31 +22,30 @@ int main(void)
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	short t, ans;
-	;
+	short t, i, ans;
 	int a, b, xk, yk, xq, yq;
-	vector<pair<short, short>> directions = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}, {0, 0}};
-	set<pair<int, int>> kap, qap;
+	vector<pair<short, short>> directions = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+	set<pair<short, short>> posk, posq;
 
 	cin >> t;
 	while (t--)
 	{
 		cin >> a >> b >> xk >> yk >> xq >> yq;
+		posk.clear();
+		posq.clear();
 		ans = 0;
-		kap.clear();
-		qap.clear();
-		for (pair<int, int> direction : directions)
+		for (i = 0; i < 4; i++)
 		{
-			kap.insert({xk + direction.first * a, yk + direction.second * b});
-			qap.insert({xq + direction.first * a, yq + direction.second * b});
+			posk.insert({xk + a * directions[i].first, yk + b * directions[i].second});
+			posq.insert({xq + a * directions[i].first, yq + b * directions[i].second});
 		}
-		for (pair<int, int> direction : directions)
+		for (i = 0; i < 4; i++)
 		{
-			kap.insert({xk + direction.first * b, yk + direction.second * a});
-			qap.insert({xq + direction.first * b, yq + direction.second * a});
+			posk.insert({xk + b * directions[i].first, yk + a * directions[i].second});
+			posq.insert({xq + b * directions[i].first, yq + a * directions[i].second});
 		}
-		for (pair<int, int> kapi : kap)
-			qap.find(kapi) != qap.end() ? ans++ : ans;
+		for (pair<short, short> posk_en : posk)
+			posq.find(posk_en) != posq.end() ? ans++ : 0;
 		cout << ans << '\n';
 	}
 	return (EXIT_SUCCESS);
